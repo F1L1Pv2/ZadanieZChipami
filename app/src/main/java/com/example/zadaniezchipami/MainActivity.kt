a9f7a0b76af69379c02ce1cd0011757d7afa7834
F1L1P <mlodzik.filip@wp.pl>
2023-03-24 17:52:52 +0100

    Pierwszy commit + ui

---
+++ app/src/main/java/com/example/zadaniezchipami/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.zadaniezchipami
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
